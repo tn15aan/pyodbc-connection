ce10a6a373baa7d9808879f4e7146ac73dbbd243
Thomas Nguyen <TNguyen@spartaglobal.com>
2019-12-10 16:58:58 +0000

    Retrieved data from database

--- db_connect.py
+++ db_connect.py
@@ -1,16 +1,32 @@
 import pyodbc
 
+# These are our variables to connect
 server = 'localhost,1433'
 database = 'Northwind'
 username = 'SA'
 password = 'Passw0rd2018'
 
+# Making the connection
 docker_Northwind = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
+# Making a cursor
 cursor = docker_Northwind.cursor()
 
-cursor.execute("SELECT @@version")
+# Executing some SQL commands
+cursor.execute("SELECT * FROM Customers WHERE city LIKE 'London'")
+
+# Fetching data from the executed SQL command and printing
 row = cursor.fetchone()
-
-
 print(row)
+
+# Cursors maintaining state
+print(cursor.fetchone())
+
+# Accessing specific data
+    # use the column name as an attribute of the entry
+row = cursor.fetchone()
+print(row)
+print(row.CompanyName, row.ContactName)
+
+print(type(cursor))
+print(type(row))
